62c3d093d2849072f38424b1a56424c2a7710fa7
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-07-23 23:54:52 +0300

    ADD: Our friends page

--- src/pages/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage.jsx
@@ -1,34 +1,13 @@
-import { useState } from 'react';
-import ModalWindow from 'components/shared/ModalWindow';
+import FriendsCards from '../components/OurFriends/FriendsCards';
+import css from '../styles/OurFriendsPage.module.css';
+import sponsors from '../sponsors.json';
 
 export default function OurFriendsPage() {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
   return (
-    <div>
-      <h1>Our Friends</h1>
-      <button onClick={openModal}>Open Modal</button>
-
-      {modalOpen && (
-        <ModalWindow onClose={closeModal}>
-          <>
-            <h2>Attention</h2>
-            <p>
-              We would like to remind you that certain functionality is
-              available only to authorized users.If you have an account, please
-              log in with your credentials. If you do not already have an
-              account, you must register to access these features.
-            </p>
-          </>
-        </ModalWindow>
-      )}
+    //додати однотонний фон, коли буде готовий компонент
+    <div className={css.container}>
+      <h1 className={css.title}>Our Friends</h1>
+      <FriendsCards sponsors={sponsors} />
     </div>
   );
 }
