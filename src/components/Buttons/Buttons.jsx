6f1f1ba5f153e8f714c314e8be4648b3e024a4f3
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-07-24 16:11:45 +0300

    btn адаптив

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ReactComponent as Plus } from "../../images/plus-small.svg";
+import { ReactComponent as PlusSmall } from "../../images/plus-small.svg";
+import { ReactComponent as Plus } from "../../images/plus.svg";
 import { ReactComponent as Filter } from "../../images/filters-3.svg";
 import css from "./Buttons.module.css"
 
@@ -12,12 +13,17 @@ const Buttons = () => {
             <button type="button" className={css.btn} autoFocus>sell</button>  
             <button type="button" className={css.btn}>lost/found</button>
             <button type="button" className={css.btn}>in good hands</button>
-            <button type="button" className={css.btn}>favorite ads</button>
-            <button type="button" className={css.btn}>my ads</button>
+            <div className={css['btn-auth']}>
+                <button type="button" className={css.btn}>favorite ads</button>
+                <button type="button" className={css.btn}>my ads</button>
+            </div>
+            
          </div> 
          <div className={css['sub-buttons']}>
-            <button type="button" className={css['btn-filter']}>Filter <span className={css['icon-filter']}><Filter /></span></button>  
-            <button type="button" className={css['btn-add']}>Add Pet <span className={css['icon-plus']}><Plus /></span></button>                
+            <button type="button" className={css['btn-filter']}>Filter <span className={css['icon-filter']}><Filter /></span></button> 
+            <button type="button" className={css['btn-mob-filter']}><span className={css['icon-filter']}><Filter /></span></button> 
+            <button type="button" className={css['btn-add']}>Add Pet <span className={css['icon-plus']}><PlusSmall /></span></button> 
+            <button type="button" className={css['btn-mob-add']}> <span className={css['icon']}><Plus /></span>Add pet</button>  
          </div>             
       </div>
     
