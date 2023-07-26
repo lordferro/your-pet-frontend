57e994a68461b8fec6ac81d590929628aa5024c7
artyomakulov <artyomakulov@gmail.com>
2023-07-26 17:56:59 +0300

    add validation and styles

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,44 +1,65 @@
 import React from 'react';
-import css from './Login.module.css';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operation';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { validationSchema } from './LoginValidation';
+
+import css from './Login.module.css';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(logIn(values));
+    resetForm();
   };
 
   return (
     <div className={css.login_container}>
-      <form
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={validationSchema}
         onSubmit={handleSubmit}
-        autoComplete="off"
-        className={css.login_form}
       >
-        <h2 className={css.login_title}>Login</h2>
-        <input type="email" name="email" placeholder="Email" className={css.input} />
-        <input type="password" name="password" placeholder="Password" className={css.input} />
-        <button type="submit" className={css.button}>
-          Login
-        </button>
-        <p className={css.refTitleToReg}>
-          Don't have an account?{' '}
-          <Link to="/register" className={css.refLogin}>
-            Register
-          </Link>
-        </p>
-      </form>
+        <Form className={css.login_form} autoComplete="off">
+          <h2 className={css.login_title}>Login</h2>
+          <div className={css.inputContainer}>
+            <Field
+              type="email"
+              name="email"
+              placeholder="Email"
+              className={css.input}
+            />
+            <ErrorMessage name="email" component="div" className={css.error} />
+          </div>
+          <div className={css.inputContainer}>
+            <Field
+              type="password"
+              name="password"
+              placeholder="Password"
+              className={css.input}
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className={css.error}
+            />
+          </div>
+          <button type="submit" className={css.button}>
+            Login
+          </button>
+          <p className={css.refTitleToReg}>
+            Don't have an account?{' '}
+            <Link to="/register" className={css.refLogin}>
+              Register
+            </Link>
+          </p>
+        </Form>
+      </Formik>
     </div>
   );
 };
