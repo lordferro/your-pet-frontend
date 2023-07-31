bfcb7f951ae23e13752fdee774e2c68cd05282f5
Alex Pan <alxtouch@gmail.com>
2023-07-31 18:02:33 +0300

    add css module styles and change some paths

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/slice';
+import newsReducer from './news/newsSlice';
 
 
 const authPersistConfig = {
@@ -22,6 +23,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
+    news: newsReducer,
   },
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware({
