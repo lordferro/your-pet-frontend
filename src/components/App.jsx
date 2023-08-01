499e3e98d87f9a3c0864a3a3c13f08991bf59118
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-08-01 18:02:51 +0200

    clean

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,8 +9,6 @@ import Addpet from 'pages/Add-pet/Add-pet';
 import { refreshUser } from 'redux/auth/operation';
 import { useAuth } from 'hooks';
 import { Layout } from './Layout';
-import axios from 'axios';
-import { fetchNotices } from 'redux/notices/operations';
 const MainPage = lazy(() => import('pages/MainPage'));
 const NewsPage = lazy(() => import('pages/NewsPage'));
 const NoticesPage = lazy(() => import('pages/NoticesPage/NoticesPage'));
@@ -22,7 +20,7 @@ const UserPage = lazy(() => import('pages/UserPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
-  const { isRefreshing } = useAuth();
+  const { isRefreshing, user } = useAuth();
   useEffect(() => {
     dispatch(refreshUser());
 
@@ -31,7 +29,7 @@ export const App = () => {
   if (isRefreshing) {
     return <Loader />;
   }
-
+console.log(user)
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
