26bc9a84fe9650a26a25b60f37dd69826fbc11f8
OlhaHobchuk <111050433+OlhaHobchuk@users.noreply.github.com>
2023-08-02 18:41:18 +0300

    Додає курсор-лапки і рендерить по умові компоненти

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,6 +3,7 @@ import BigLogo from '../../images/logo-desktop.svg';
 import MobileLogo from '../../images/Logo-mobile.svg';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import css from './Logo.module.css';
 
 const Logo = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -11,9 +12,19 @@ const Logo = () => {
     <div>
       <Link to="/main">
         {isMobile ? (
-          <img src={MobileLogo} alt="Mobile logo" width={116} />
+          <img
+            src={MobileLogo}
+            className={css.logo}
+            alt="Mobile logo"
+            width={116}
+          />
         ) : (
-          <img src={BigLogo} alt="Desktop logo" width={162} />
+          <img
+            src={BigLogo}
+            className={css.logo}
+            alt="Desktop logo"
+            width={162}
+          />
         )}
       </Link>
     </div>
--- src/components/Notice/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/Notice/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -15,7 +15,6 @@ export const NoticesCategoriesList = ({ cards }) => {
   const { user } = useAuth();
   useEffect(() => {
     setFavoritesPets(user.favoritePets);
-    console.log(favoritesPets)
   }, [favoritesPets, user.favoritePets]);
 
   const handelDeleteFavorite = id => {
