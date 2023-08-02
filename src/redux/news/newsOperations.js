6efac9137afc2702e8ff9f8d2fa17ec765fe6934
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-08-02 08:22:34 +0200

    fix done

--- src/redux/news/newsOperations.js
+++ src/redux/news/newsOperations.js
@@ -1,20 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// export const instance = axios.create({
-//   baseURL: 'https://your-pet.onrender.com/api',
-// });
-
 export const fetchNews = createAsyncThunk(
   'news/all',
   async ({ searchQuery, page }, thunkAPI) => {
     try {
-      const response = await axios.get('/news', {
-        params: {
-          search: searchQuery,
-          page,
-        },
-      });
+      const response = await axios.get(
+        'https://your-pet.onrender.com/api/news',
+        {
+          params: {
+            search: searchQuery,
+            page,
+          },
+        }
+      );
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
